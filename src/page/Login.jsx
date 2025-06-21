c7ac0c97a599602ba73270723e17b4b65621da65
Fat1512 <letanphat15122004@gmail.com>
2025-06-21 17:55:14 +0700

    asd

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -47,7 +47,7 @@ function Login() {
           onChange={(e) => setPassword(e.target.value)}
         />
         <button type="submit" className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600">
-          Login !
+          Login
         </button>
       </form>
     </div>
